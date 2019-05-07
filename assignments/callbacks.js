16125ff400ef1a70ec192433f6590cad8ee7921a
Winnie Song <winniesong0121@gmail.com>
2019-05-07 17:41:37 -0400

    MVP finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -30,28 +30,48 @@ function getLength(arr, cb) {
    return cb(arr.length);
 }
 
-getLength(items, length => console.log(length));
+getLength(items, function(length){
+  console.log(length);
+});
 
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  
+  return cb(arr[arr.length-1]);
 }
+
+last(items, function(x){
+  console.log(x);
+});
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  // return add(x, y);
+  return cb(x + y);
 }
 
+sumNums(1 ,2, function(total){
+  console.log(total)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(1, 2, function(total){
+  console.log(total);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item));
 }
+
+contains("Pencil", items, function(isIncluded){
+  console.log(isIncluded);
+})
 
 /* STRETCH PROBLEM */
 
